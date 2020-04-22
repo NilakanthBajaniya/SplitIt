c2d3488b1ada9c65df6e01cdc990416c478bc74f
Nil1510 <nilbajaniya@gmail.com>
2020-04-22 01:12:25 -0400

    currency conversion data fetch

---
+++ js/library.js
@@ -0,0 +1,138 @@
+/*
+Author: Nilkanth Bajaniya
+*/
+
+//generic code to perform add, edit, update, remove operation on the data stored in the local storage.
+var Utility = {
+    //to add item or array in the local-storage to the specific key
+    add: function (key, values) {
+
+        var data = localStorage.getItem(key);
+
+        if (data != null) {
+
+            data = JSON.parse(data);
+            var lastId = data[data.length - 1]["Id"];
+
+            if (values.length != undefined) {
+
+                for (var i = 0; i < values.length; i++) {
+
+                    values[i]["Id"] = lastId + (i + 1);
+                    data.push(values[i]);
+                }
+            } else {
+
+                values["Id"] = lastId + 1;
+                data.push(values);
+            }
+
+            localStorage.setItem(key, JSON.stringify(data));
+        } else {
+
+            localStorage.setItem(key, JSON.stringify(values));
+        }
+    },
+    //update the one of the value of specific key
+    update: function (key, Id, propertyName, newPropertyValue) {
+
+        var data = localStorage.getItem(key);
+
+        if (data != null) {
+
+            data = JSON.parse(data);
+
+            for (var i = 0; i < data.length; i++) {
+
+                if (data[i]["Id"].toString() === Id.toString()) {
+
+                    data[i][propertyName] = newPropertyValue;
+                }
+            }
+
+            localStorage.setItem(key, JSON.stringify(data));
+        } else return;
+    },
+    //to remove the object from specific key
+    remove: function (key, value) {
+
+        var data = localStorage.getItem(key);
+
+        if (data != null) {
+
+            data = JSON.parse(data);
+
+            for (var i = 0; i < data.length; i++) {
+
+                if (data[i]["Id"].toString() === value["Id"].toString()) {
+
+                    data.splice(i, 1);
+                    this.removeKey("tasks");
+                    localStorage.setItem(key, JSON.stringify(data));
+                    return true;
+                }
+            }
+
+            return false;
+        } else {
+
+            return false;
+        }
+    },
+    //to find the object have same  property value
+    search: function (key, propertyName, propertyValue) {
+
+        var data = localStorage.getItem(key);
+        var returnData = [];
+
+        if (data != null) {
+
+            data = JSON.parse(data);
+
+            for (var i = 0; i < data.length; i++) {
+
+                if (data[i][propertyName].toString() === propertyValue.toString()) {
+
+                    returnData.push(data[i]);
+                }
+            }
+
+            if (returnData.length > 0)
+                return returnData;
+            else return;
+        } else return;
+    },
+    //fetch data of the specific key
+    getKeyData: function (key) {
+
+        var data = localStorage.getItem(key);
+
+        if (data != null) {
+
+            return JSON.parse(data);
+        } else {
+
+            return data;
+        }
+    },
+    //remove specific key from the local-storage
+    removeKey: function (key) {
+        localStorage.removeItem(key);
+    }
+};
+
+//validating the user login
+function validateUser() {
+
+    if (Utility.getKeyData("login") == null) {
+
+        location.href = "login.html";
+    }
+}
+
+//to logout user from the application
+function logout() {
+    localStorage.removeItem("login");
+    location.reload(true);
+}
+
---
+++ js/startup.js
@@ -0,0 +1,34 @@
+(function () {
+
+
+    //function to retrieve the currency conversion rates 
+    var url = 'https://prime.exchangerate-api.com/v5/50d8b6b9d8ac1e40db7454a3/latest/CAD';
+
+    //request object to api for conversion rates.
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, true);
+
+    xhttp.onreadystatechange = function () {
+
+        try {
+
+            //validating the response
+            if (this.readyState == 4 && this.status == 200) {
+
+                var result = JSON.parse(this.response);
+                var rates = result.conversion_rates;
+
+                if (result != undefined && result != null && result.result === "success" && rates != undefined) {
+
+                    //storing the conversion Rates in the local-storage
+                    Utility.add("conversionRates", rates);
+                };
+            }
+        } catch (Exception) {
+
+            location.reload(true);
+        }
+    };
+
+    xhttp.send();
+})();
